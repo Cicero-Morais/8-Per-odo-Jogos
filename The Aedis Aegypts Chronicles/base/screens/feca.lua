48ad6f28a4d024a81eea811d0bfcc29fbe44399c
elcio <elcio20@live.com>
2016-03-30 22:40:42 -0300

    tela inicial feca

---
+++ The Aedis Aegypts Chronicles/base/screens/feca.lua
@@ -0,0 +1,110 @@
+return {
+  version = "1.1",
+  luaversion = "5.1",
+  tiledversion = "0.15.2",
+  orientation = "orthogonal",
+  renderorder = "right-down",
+  width = 20,
+  height = 20,
+  tilewidth = 32,
+  tileheight = 32,
+  nextobjectid = 1,
+  properties = {},
+  tilesets = {
+    {
+      name = "2a7a7tl",
+      firstgid = 1,
+      tilewidth = 640,
+      tileheight = 475,
+      spacing = 0,
+      margin = 0,
+      image = "2a7a7tl.png",
+      imagewidth = 938,
+      imageheight = 476,
+      tileoffset = {
+        x = 0,
+        y = 0
+      },
+      properties = {},
+      terrains = {},
+      tilecount = 1,
+      tiles = {}
+    },
+    {
+      name = "2a7a7tl",
+      firstgid = 2,
+      tilewidth = 32,
+      tileheight = 32,
+      spacing = 0,
+      margin = 0,
+      image = "2a7a7tl.png",
+      imagewidth = 938,
+      imageheight = 476,
+      tileoffset = {
+        x = 0,
+        y = 0
+      },
+      properties = {},
+      terrains = {},
+      tilecount = 406,
+      tiles = {}
+    },
+    {
+      name = "season_and_environment_rpg_tiles_by_seraawah-d4l9d1x",
+      firstgid = 408,
+      tilewidth = 47,
+      tileheight = 67,
+      spacing = 0,
+      margin = 0,
+      image = "season_and_environment_rpg_tiles_by_seraawah-d4l9d1x.png",
+      imagewidth = 448,
+      imageheight = 272,
+      tileoffset = {
+        x = 0,
+        y = 0
+      },
+      properties = {},
+      terrains = {},
+      tilecount = 36,
+      tiles = {}
+    }
+  },
+  layers = {
+    {
+      type = "tilelayer",
+      name = "Camada de Tiles 1",
+      x = 0,
+      y = 0,
+      width = 20,
+      height = 20,
+      visible = true,
+      opacity = 1,
+      offsetx = -90.6667,
+      offsety = -48,
+      properties = {},
+      encoding = "lua",
+      data = {
+        264, 264, 264, 264, 264, 264, 264, 264, 264, 264, 264, 264, 264, 264, 264, 249, 250, 251, 252, 88,
+        264, 264, 264, 264, 264, 264, 264, 264, 264, 264, 264, 264, 264, 264, 264, 278, 279, 280, 281, 88,
+        264, 264, 264, 292, 264, 264, 292, 263, 264, 292, 264, 264, 292, 264, 264, 307, 308, 309, 310, 88,
+        322, 323, 323, 321, 322, 323, 321, 322, 323, 321, 322, 323, 321, 322, 323, 336, 337, 338, 339, 88,
+        44, 45, 46, 350, 351, 352, 350, 354, 355, 350, 351, 352, 350, 351, 352, 365, 366, 367, 368, 88,
+        0, 0, 0, 46, 44, 45, 46, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 87, 88,
+        44, 0, 0, 46, 44, 44, 45, 46, 0, 0, 44, 45, 46, 0, 0, 0, 0, 0, 0, 0,
+        44, 45, 46, 0, 0, 0, 0, 0, 44, 45, 44, 44, 45, 46, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 44, 45, 46, 46, 0, 0, 0, 44, 45, 46, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 44, 45, 46, 46, 45, 46, 46, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+      }
+    }
+  }
+}
